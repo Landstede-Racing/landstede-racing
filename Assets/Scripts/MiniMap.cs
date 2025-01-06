791dda72b1a7fae6bbf44b854da805d7afd5d01e
Student Levi Rosheuvel <lrosheuv6991@student.landstede.nl>
2025-01-06 12:09:56 +0100

    Minimap Done

--- Assets/Scripts/MiniMap.cs
+++ Assets/Scripts/MiniMap.cs
@@ -7,28 +7,42 @@ public class MiniMap : MonoBehaviour
 
     void Start()
     {
+        Debug.Log("MiniMap :O");
         lineRenderer = GetComponent<LineRenderer>();
         TrackPath = this.gameObject;
 
         int path_num = TrackPath.transform.childCount;
-        lineRenderer.positionCount = path_num;
-
-        for (int i = 0; i < path_num; i++)
+        if (path_num > 0)
         {
-            lineRenderer.SetPosition(i, new Vector3(TrackPath.transform.GetChild(i).transform.position.x,
-            50, TrackPath.transform.GetChild(i).transform.position.z));
+            lineRenderer.positionCount = path_num;
+
+            for (int i = 0; i < path_num; i++)
+            {
+                Vector3 childPosition = TrackPath.transform.GetChild(i).transform.position;
+                lineRenderer.SetPosition(i, new Vector3(childPosition.x, 50, childPosition.z));
+            }
+
+            lineRenderer.SetPosition(path_num - 1, lineRenderer.GetPosition(0));
+        }
+        else
+        {
+            Debug.LogWarning("TrackPath has no children.");
         }
 
-        lineRenderer.SetPosition(path_num, lineRenderer.GetPosition(0)); //for aligning 
-
-        lineRenderer.startWidth = 10f;
-        lineRenderer.endWidth = 10f;
-
-        // lineRenderer.numCornerVertices = 10;
+        // lineRenderer.startWidth = 10f;
+        // lineRenderer.endWidth = 10f;
     }
 
-    void Update()
+    public void SetSize(float startWidth, float endWidth)
     {
-
+        if (lineRenderer != null)
+        {
+            lineRenderer.startWidth = startWidth;
+            lineRenderer.endWidth = endWidth;
+        }
+        else
+        {
+            Debug.LogError("LineRenderer is not assigned.");
+        }
     }
 }
