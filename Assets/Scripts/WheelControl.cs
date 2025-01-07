c04ad27f9322e58b4681e9b945329f4e4ceacf97
Student Daimy Wisselink <dwisseli1252@student.landstede.nl>
2025-01-07 11:17:48 +0100

    geen idee wat ik gister heb gedaan lol

--- Assets/Scripts/WheelControl.cs
+++ Assets/Scripts/WheelControl.cs
@@ -3,6 +3,7 @@ using UnityEngine;
 public class WheelControl : MonoBehaviour
 {
     public Transform wheelModel;
+    private DamagablePart damagablePart;
 
     [HideInInspector] public WheelCollider WheelCollider;
 
@@ -19,6 +20,7 @@ public class WheelControl : MonoBehaviour
     private void Start()
     {
         WheelCollider = GetComponent<WheelCollider>();
+        damagablePart = GetComponent<DamagablePart>();
     }
 
     // Update is called once per frame
@@ -29,5 +31,23 @@ public class WheelControl : MonoBehaviour
         WheelCollider.GetWorldPose(out position, out rotation);
         wheelModel.transform.position = position;
         wheelModel.transform.rotation = rotation;
+
+        // Damage from driving (get force from ground hit, and calculate damage using that)
+        if(WheelCollider.isGrounded) {
+            WheelCollider.GetGroundHit(out WheelHit hit);
+
+            // damagablePart.currentDamage += (int)collision.impulse.magnitude;
+            // Debug.Log("-----------------------------");
+            // Debug.Log("Part: " + damagablePart.part.name);
+            // Debug.Log("currentDamage: " + damagablePart.currentDamage);
+            // Debug.Log("velocity: " + collision.impulse.magnitude);
+            // Debug.Log("-----------------------------");
+            
+            if (damagablePart.currentDamage >= damagablePart.maxDamage)
+            {
+                Debug.Log("Part: " + damagablePart.part.name + " is destroyed");
+                Destroy(gameObject);
+            }
+        }
     }
 }
