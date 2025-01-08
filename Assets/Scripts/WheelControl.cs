e6786617493a251f6199b4e076c71d4f6f755c2c
Student Daimy Wisselink <dwisseli1252@student.landstede.nl>
2025-01-08 14:20:35 +0100

    Refactor WheelControl to manage damage using new properties and improve logging

--- Assets/Scripts/WheelControl.cs
+++ Assets/Scripts/WheelControl.cs
@@ -1,9 +1,14 @@
 using UnityEngine;
+using UnityEngine.Rendering;
 
 public class WheelControl : MonoBehaviour
 {
     public Transform wheelModel;
-    private DamagablePart damagablePart;
+
+    public Part part;
+    public int maxDamage = 250;
+    public int currentDamage;
+
 
     [HideInInspector] public WheelCollider WheelCollider;
 
@@ -20,7 +25,6 @@ public class WheelControl : MonoBehaviour
     private void Start()
     {
         WheelCollider = GetComponent<WheelCollider>();
-        damagablePart = GetComponent<DamagablePart>();
     }
 
     // Update is called once per frame
@@ -33,19 +37,23 @@ public class WheelControl : MonoBehaviour
         wheelModel.transform.rotation = rotation;
 
         // Damage from driving (get force from ground hit, and calculate damage using that)
-        if(WheelCollider.isGrounded) {
+        if (WheelCollider.isGrounded)
+        {
             WheelCollider.GetGroundHit(out WheelHit hit);
 
-            // damagablePart.currentDamage += (int)collision.impulse.magnitude;
-            // Debug.Log("-----------------------------");
-            // Debug.Log("Part: " + damagablePart.part.name);
-            // Debug.Log("currentDamage: " + damagablePart.currentDamage);
-            // Debug.Log("velocity: " + collision.impulse.magnitude);
-            // Debug.Log("-----------------------------");
-            
-            if (damagablePart.currentDamage >= damagablePart.maxDamage)
+            int force = (int)hit.force;
+            if (force > 0)
             {
-                Debug.Log("Part: " + damagablePart.part.name + " is destroyed");
+                Debug.Log("-----------------------------");
+                Debug.Log("Part: " + part.name);
+                Debug.Log("currentDamage: " + currentDamage);
+                Debug.Log("force: " + force);
+                Debug.Log("-----------------------------");
+            }
+
+            if (currentDamage >= maxDamage)
+            {
+                Debug.Log("Part: " + part.name + " is destroyed");
                 Destroy(gameObject);
             }
         }
