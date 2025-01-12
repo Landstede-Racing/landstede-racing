77cb3c71e35ccc436db926adaf53e6d89d511088
_Coln <csoer1947@student.landstede.nl>
2025-01-12 15:50:34 +0100

    Added defaults to TerrainInfo

--- Assets/Scripts/TerrainInfo.cs
+++ Assets/Scripts/TerrainInfo.cs
@@ -2,9 +2,9 @@ using UnityEngine;
 
 public class TerrainInfo : MonoBehaviour
 {
-    public bool vibration;
-    public float vibrationFrequency;
-    public float vibrationIntensity;
-    public float gripMultiplier;
-    public float damageMultiplier;
+    public bool vibration = false;
+    public float vibrationFrequency = 0;
+    public float vibrationIntensity = 0;
+    public float gripMultiplier = 1;
+    public float damageMultiplier = 1;
 }
