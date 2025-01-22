7d734c68726a3e190c81eef250b3d290b13c3b0b
Student Daimy Wisselink <dwisseli1252@student.landstede.nl>
2025-01-22 13:43:21 +0100

    Colin is saii

--- Assets/Scripts/SettingsController.cs
+++ Assets/Scripts/SettingsController.cs
@@ -1,6 +1,5 @@
 public static class SettingsController
 {
-    // Colin - Jij luie homo dit kon je zelf ook veranderen
     public static string DeviceController { get; set; } = "steeringWheel"; //for using ulta mooie stuurtje oehleh
     // public static string DeviceController { get; set; } = "gamepadController"; // Controller (ps4)
     // public static string DeviceController { get; set; } = "keyboardController";  // Keyboard
