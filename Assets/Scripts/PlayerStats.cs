8d58f1a60bf33e0820b58342caca74480bdce818
Kaaspower <118729858+Kaaspower@users.noreply.github.com>
2025-01-22 09:59:32 +0100

    ik moest nog main pullen, rip mij

--- Assets/Scripts/PlayerStats.cs
+++ Assets/Scripts/PlayerStats.cs
@@ -7,9 +7,6 @@ using LandstedeRacing.Types;
 
 public class PlayerStats : NetworkBehaviour
 {
-    [SerializeField]
-    private float pushPower = 20f;
-
     [SerializeField] private Rigidbody rb;
 
     [DoNotSerialize] public Stopwatch stopwatch = new();
@@ -32,22 +29,9 @@ public class PlayerStats : NetworkBehaviour
         time = Random.Range(0f, 3f);
         tire = RandomTire(Random.Range(0, 2));
     }
-    
-    // private void Update()
-    // {
-    //     if (!rb || !IsServer) return;
-    //     if (Input.GetKeyDown(KeyCode.M))
-    //     {
-    //         _stopwatch.Start();
-    //         rb.AddForce(-pushPower, 0f, 0f, ForceMode.Impulse);
-    //         Debug.Log("Adding force to coob");
-    //     }
-    // }
 
     public override void OnNetworkSpawn()
     {
-        // Debug.Log(NetworkObjectId);
-        
         playerTimings.Add(new(NetworkObjectId, 999999999, 0, 0));
 
         name = NetworkObjectId.ToString();
@@ -59,7 +43,7 @@ public class PlayerStats : NetworkBehaviour
         playerTimings.Add(playerTiming);
         stopwatch.Restart();
         totalDriveTime = totalDriveTime + stopwatch.ElapsedMilliseconds;
-        Debug.Log(playerTimings[playerTimings.Count - 1].NetworkId + ", " + playerTimings[playerTimings.Count - 1].Timing);
+        // Debug.Log(playerTimings[playerTimings.Count - 1].NetworkId + ", " + playerTimings[playerTimings.Count - 1].Timing);
     }
     
     public void NewTiming(int sectorId, bool lapUp)
@@ -68,7 +52,7 @@ public class PlayerStats : NetworkBehaviour
         playerTimings.Add(playerTiming);
         stopwatch.Restart();
         totalDriveTime = totalDriveTime + stopwatch.ElapsedMilliseconds;
-        Debug.Log(playerTimings[playerTimings.Count - 1].NetworkId + ", " + playerTimings[playerTimings.Count - 1].Timing);
+        // Debug.Log(playerTimings[playerTimings.Count - 1].NetworkId + ", " + playerTimings[playerTimings.Count - 1].Timing);
     }
 
     private static string RandomTire(int tireIndex)
